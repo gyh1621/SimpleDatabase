b8bab89934eb13a2622457eb1db8caeae40d5654
gyh1621 <cngyh96@gmail.com>
2020-01-13 12:48:51 -0800

    implement FileHandle class: not tested

--- rbf/pfm.cc
+++ rbf/pfm.cc
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "pfm.h"
 
 PagedFileManager &PagedFileManager::instance() {
@@ -33,26 +34,96 @@ FileHandle::FileHandle() {
     readPageCounter = 0;
     writePageCounter = 0;
     appendPageCounter = 0;
+    totalPage = 0;
+    handle = nullptr;
 }
 
 FileHandle::~FileHandle() = default;
 
+RC FileHandle::readHiddenPage(){
+    void *data = malloc(sizeof(unsigned) * 4 + sizeof(char));
+    readPage(-1, data);
+    if (*((char *) data) != 'Y') return 1;
+    readPageCounter = (int) *((char *) data + sizeof(char));
+    writePageCounter = (int) *((char *) data + sizeof(unsigned));
+    appendPageCounter = (int) *((char *) data + sizeof(unsigned) * 2);
+    totalPage = (int) *((char *) data + sizeof(unsigned) * 3);
+    free(data);
+    return 0;
+}
+
+RC FileHandle::writeHiddenPage() {
+    if (handle == nullptr) return 1;
+    void *data = malloc(sizeof(unsigned) * 3 + sizeof(char));
+    *((char *) data) = 'Y';
+    *((char *) data + sizeof(char)) = readPageCounter;
+    *((char *) data + sizeof(char) + sizeof(unsigned)) = writePageCounter;
+    *((char *) data + sizeof(char) + sizeof(unsigned) * 2) = appendPageCounter;
+    *((char *) data + sizeof(char) + sizeof(unsigned) * 3) = totalPage;
+    writePage(-1, data);
+    free(data);
+    return 0;
+}
+
+RC FileHandle::setHandle(std::fstream &f) {
+    if (handle != nullptr) {
+        // a file is already opened
+        return 1;
+    }
+    handle = &f;
+    // detect if this file was initialized
+    if (readHiddenPage() != 0) {
+        // write hidden page
+        writeHiddenPage();
+    }
+    return 0;
+}
+
+RC FileHandle::releaseHandle() {
+    if (handle == nullptr) {
+        // no file open
+        return 1;
+    } else {
+        // write counters and total page number
+        writeHiddenPage();
+        handle->close();
+        return 0;
+    }
+}
+
 RC FileHandle::readPage(PageNum pageNum, void *data) {
-    return -1;
+    // TODO
+    // add test: detect page num is valid
+    pageNum += 1;  // because the hidden page is the first page
+    handle->seekp(pageNum * PAGE_SIZE, ios::beg);
+    handle->read((char*) data, PAGE_SIZE);
+    readPageCounter++;
+    return 0;
 }
 
 RC FileHandle::writePage(PageNum pageNum, const void *data) {
-    return -1;
+   pageNum += 1;
+   handle->seekp(pageNum * PAGE_SIZE, ios::beg);
+   handle->write((const char*) data, PAGE_SIZE);
+   writePageCounter++;
+   return 0;
 }
 
 RC FileHandle::appendPage(const void *data) {
-    return -1;
+    totalPage += 1;
+    handle->seekp(0, ios::end);
+    handle->write((const char *) data, PAGE_SIZE);
+    appendPageCounter++;
+    return 0;
 }
 
 unsigned FileHandle::getNumberOfPages() {
-    return -1;
+    return totalPage;
 }
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
-    return -1;
+    readPageCount = this->readPageCounter;
+    writePageCount = this->writePageCounter;
+    appendPageCount = this->appendPageCounter;
+    return 0;
 }
--- rbf/pfm.h
+++ rbf/pfm.h
@@ -1,3 +1,5 @@
+#include <fstream>
+
 #ifndef _pfm_h_
 #define _pfm_h_
 
@@ -28,14 +30,54 @@ protected:
 };
 
 class FileHandle {
+private:
+    std::fstream *handle;
+
+    /* write counters and page number to a hidden page
+     *
+     * Hidden page format:
+     * char     unsigned int        unsigned int        unsigned int        unsigned int
+     * IsInit   readPageCounter     writePageCounter    appendPageCounter   pageNumber
+     *
+     * Return:
+     *  0: success
+     *  1: fail, currently only when no file is open
+     */
+    RC writeHiddenPage();
+
+    /* read counters from the hidden page
+     * Return:
+     *  0: success, all counters updated from the file
+     *  1: fail, file not contain a hidden page
+     */
+    RC readHiddenPage();
+
 public:
     // variables to keep the counter for each operation
     unsigned readPageCounter;
     unsigned writePageCounter;
     unsigned appendPageCounter;
 
+    // page number, don't count hidden page
+    PageNum totalPage;
+
     FileHandle();                                                       // Default constructor
     ~FileHandle();                                                      // Destructor
+
+    /* Set file handle
+     * Return:
+     *  0: set successfully
+     *  1: a file is already opened
+    */
+    RC setHandle(std::fstream &f);
+
+    /* Release file handle
+     * Return:
+     *  0: release successfully
+     *  1: no file is opened
+     */
+    RC releaseHandle();
+
 
     RC readPage(PageNum pageNum, void *data);                           // Get a specific page
     RC writePage(PageNum pageNum, const void *data);                    // Write a specific page
